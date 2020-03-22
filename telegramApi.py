64ae3e53c75be603cbadc8b8a9ce8f8453018cc7
Federico PAVAN <federico90pavan@gmail.com>
2020-03-22 19:25:32 +0100

    removed sensitive data

--- telegramApi.py
+++ telegramApi.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
 import requests
-import time
+import sys
 
 
 def callTelegram(text):
     base_url = "https://api.telegram.org/"
     call_type = "bot"
-    caller_id = "1147116456:AAEaa8Knqpr7rbVgEKuoVV5UQa8eQooL5bg"
+    credentials = open("credentials.txt","r").read().split()
+    print(credentials)
     verb = "/sendMessage"
-    channel_id = "@limortacciForMyBot"
 
     url = base_url + call_type + caller_id + verb
 
     query = "?chat_id="+channel_id+"&text="+text
     r = requests.post(url = url+ query)
+
+if __name__ == "__main__":
+    callTelegram(sys.argv[1])
